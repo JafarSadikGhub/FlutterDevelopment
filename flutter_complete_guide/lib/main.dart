34378d140897676bc96fbb05dca3db2aaedef398
jafar95 <jafar.sadik@northsouth.edu>
2020-10-16 04:47:12 +0600

    main dart v5

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -7,13 +7,25 @@ import 'package:flutter/material.dart';
  }*/
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion()
-  {
-    print('Answer Chosen!');
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
   }
+}
 
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+    print('Answer Chosen!');
+    print(questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +40,14 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: () =>print('Answer 2 chosen!'),
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             RaisedButton(
               child: Text('Answer 3'),

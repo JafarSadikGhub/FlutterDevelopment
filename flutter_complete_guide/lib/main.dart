82ea034a536eb423ab97b2819ebc28c3d9999168
jafar95 <jafar.sadik@northsouth.edu>
2020-10-17 02:48:50 +0600

    main dart v7+answer dart v1

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'question.dart';
-
+import './question.dart';
+import './answer.dart';
 /*void main()
  {
     runApp(MyApp()); 
@@ -44,21 +44,10 @@ class _MyAppState extends State<MyApp> {
             Question(
               questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                //...
-                print('Answer 3 chosen');
-              },
-            ),
+            
+            Answer(),
+            Answer(),
+            Answer(),
           ],
         ),
       ),

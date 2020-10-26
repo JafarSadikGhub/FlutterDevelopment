9bacedb54d316e14662a16f6677ca06b1264c6ec
jafar95 <jafar.sadik@northsouth.edu>
2020-10-26 12:01:43 +0600

    added some styling on main.dart

---
+++ daily_expense_app/lib/transaction.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+//Curly braces for named argument
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.amount,
+    @required this.date,
+  });
+}

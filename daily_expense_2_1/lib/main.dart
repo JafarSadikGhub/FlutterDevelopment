a063fa4d0f16f9bc01dbfe9c897d9073efed55e1
jafar95 <jafar.sadik@northsouth.edu>
2020-11-27 15:48:27 +0600

    End of stateless widget, starting statefull one

--- daily_expense_2_1/lib/main.dart
+++ daily_expense_2_1/lib/main.dart
@@ -28,6 +28,10 @@ class MyHomePage extends StatelessWidget {
         amount: 69.99,
         date: DateTime.now()),
   ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +40,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -45,6 +49,39 @@ class MyHomePage extends StatelessWidget {
                 child: Text('CHART'),
                 elevation: 5,
                 color: Colors.yellow[300],
+              ),
+            ),
+            Card(
+              elevation: 5,
+              color: Colors.redAccent,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                      // onChanged: (val) {
+                      //   titleInput = val;
+                      // },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                      // onChanged: (val) {
+                      //   amountInput = val;
+                      // },
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        print(titleController.text);
+                      },
+                      child: Text('Add Transaction'),
+                      textColor: Colors.green,
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
@@ -80,7 +117,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        DateFormat().format(tx.date),
+                        DateFormat('dd/MM/yy').format(tx.date),
                         style: TextStyle(color: Colors.grey, fontSize: 12),
                       ),
                     ],

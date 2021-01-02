16b6ca89e6fc086a3e095d1e20ce7eb74a5edd56
jafar95 <jafar.sadik@northsouth.edu>
2021-01-02 23:52:46 +0600

    custom date/date picker functionality added

--- daily_expense_2_1/lib/widgets/chart_bar.dart
+++ daily_expense_2_1/lib/widgets/chart_bar.dart
@@ -11,11 +11,14 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-          child: Text('\৳${spendingAmount.toStringAsFixed(0)}'),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\৳${spendingAmount.toStringAsFixed(0)}'),
+          ),
         ),
         SizedBox(
-          height: 2,
+          height: 4,
         ),
         Container(
           height: 60,
